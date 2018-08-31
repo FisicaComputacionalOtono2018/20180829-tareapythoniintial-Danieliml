e0b43087b4d494a02020f11375da4575da5f9c1e
Danieliml <42181866+Danieliml@users.noreply.github.com>
2018-08-31 11:07:15 -0500

    Add files via upload

---
+++ Area.py
@@ -0,0 +1,12 @@
+#Daniel Lima Lopez
+#30/08/2017
+#Calcular la superficie de un triángulo en función de la base y la altura
+
+import math
+
+b = input("Introduce la base: ")
+a = input("Introduce la altura: ")
+
+ar = (float(a)*float(b)/2)
+
+print ("El area del triangulo es:", ar)
